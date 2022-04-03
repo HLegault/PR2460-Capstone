1a95412222cb43c6f7ca301c1ff7d86c89062052
Heather Legault <44978187+HLegault@users.noreply.github.com>
2022-04-03 14:43:36 -0230

    Add files via upload

---
+++ Dashboard.js
@@ -0,0 +1,39 @@
+variable = "test";
+
+//Customer Info
+document.getElementById("name").innerText = "Name";
+document.getElementById("email").innerText = "Email";
+document.getElementById("address").innerText = "Address";
+document.getElementById("phone").innerText = "Phone";
+
+//Policy Info
+document.getElementById("policy-number").innerText = "Policy #";
+document.getElementById("policy-date").innerText = "Policy Date";
+document.getElementById("premimum").innerText = "Premium";
+//Home
+document.getElementById("heat-type").innerText = "Heat Type";
+document.getElementById("home-year").innerText = "Home Year";
+document.getElementById("home-value").innerText = "Home Value";
+document.getElementById("home-area").innerText = "Home Area";
+//Auto
+document.getElementById("driver-dob").innerText = "Driver DOB";
+document.getElementById("number-accidents").innerText = "# of Accidents";
+document.getElementById("vehicle-make").innerText = "Vehicle Make";
+document.getElementById("vehicle-model").innerText = "Vehicl Model";
+document.getElementById("vehicle-year").innerText = "Vehicle Year";
+
+//Quote Info
+document.getElementById("quote-number").innerText = `Policy #: ${variable}`;
+document.getElementById("quote-date").innerText = "Policy Date";
+document.getElementById("premimum-quote").innerText = "Premium";
+//Home
+document.getElementById("heat-type-quote").innerText = "Heat Type";
+document.getElementById("home-year-quote").innerText = "Home Year";
+document.getElementById("home-value-quote").innerText = "Home Value";
+document.getElementById("home-area-quote").innerText = "Home Area";
+//Auto
+document.getElementById("driver-dob-quote").innerText = "Driver DOB";
+document.getElementById("number-accidents-quote").innerText = "# of Accidents";
+document.getElementById("vehicle-make-quote").innerText = "Vehicle Make";
+document.getElementById("vehicle-model-quote").innerText = "Vehicl Model";
+document.getElementById("vehicle-year-quote").innerText = "Vehicle Year";
